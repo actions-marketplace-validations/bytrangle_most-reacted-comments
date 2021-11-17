369c7aedc6dee0a452dfebc3bbbbc7f5ff961a18
Trang Le <trang.thule@zoho.com>
2021-11-17 17:03:36 +0700

    feat(action): use input from Github Action

--- src/action.js
+++ src/action.js
@@ -1,8 +1,12 @@
 const axios = require('axios')
+const core = require('@actions/core')
+
+const DEFAULT_CHARACTER = 'dr-zoidberg'
 
 async function run() {
-  const response = await axios.get('https://futuramaapi.herokuapp.com/api/characters/dr-zoidberg/1')
-  const { data } = response
+  const character = core.getInput('character') || DEFAULT_CHARACTER
+  const response = await axios.get(`https://futuramaapi.herokuapp.com/api/characters/${character}/1`)
+  const { data = [] } = response
   const firstEntry = data[0]
   console.log(`${firstEntry.character}: ${firstEntry.quote}`)
 }
