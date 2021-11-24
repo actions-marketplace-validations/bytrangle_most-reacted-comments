f7cea7a4bca03361200e553811f172373c51e83f
Trang Le <trang.thule@zoho.com>
2021-11-24 13:19:24 +0700

    debug: print INPUT MIN ISSUE COMMENT

--- python-script.py
+++ python-script.py
@@ -1,7 +1,7 @@
 import os
 
-minIssueComment = os.environ['INPUT_MIN_ISSUE_COMMENT'] or 20
+#minIssueComment = os.environ['INPUT_MIN_ISSUE_COMMENT'] or 20
 # Input parameter passed to jobs.<job_id>.steps[*].width are available
 # as environment variable
 
-print(minIssueComment)
+print(os.environ['INPUT_MIN_ISSUE_COMMENT'])
