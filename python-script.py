97d42a51d1cb3189a184c825d6ab2f82488a315b
Trang Le <trang.thule@zoho.com>
2021-11-25 16:48:34 +0700

    feat(script): get list of issue comments

--- python-script.py
+++ python-script.py
@@ -1,11 +1,11 @@
 from os import environ, getenv
 import re
-# import requests
+import requests
 
 minIssueComment = getenv('INPUT_MIN_ISSUE_COMMENT', 20)
-print(minIssueComment)
+# print(minIssueComment)
 API_URL = getenv('GITHUB_API_URL', 'https://api.github.com')
-print(API_URL)
+# print(API_URL)
 # Input parameter passed to jobs.<job_id>.steps[*].width are available
 # as environment variable
 
@@ -16,6 +16,10 @@ issueUrl = environ['ISSUE_URL']
 # print(issueUrl)
 ISSUE_ID = re.search("issues\/(.+)", issueUrl).group(1)
 # print(ISSUE_ID)
-print(type(ISSUE_ID))
+# print(type(ISSUE_ID))
 commentApiUrl = API_URL + "/repos/" + REPO + "/issues/" + ISSUE_ID + "/comments"
 print(commentApiUrl)
+
+response = requests.get(commentApiUrl)
+json = response.json()
+print(json)
