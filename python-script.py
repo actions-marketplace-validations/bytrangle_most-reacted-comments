8bb388ef94a9e49009c26cae9bf39d4d78ce7b9f
Trang Le <trang.thule@zoho.com>
2021-11-25 11:38:00 +0700

    feat(script): get and set github api url

--- python-script.py
+++ python-script.py
@@ -3,9 +3,10 @@ from os import environ, getenv
 
 minIssueComment = getenv('INPUT_MIN_ISSUE_COMMENT', 20)
 print(minIssueComment)
+API_URL = getenv('GITHUB_API_URL', 'https://api.github.com')
+print(API_URL)
 # Input parameter passed to jobs.<job_id>.steps[*].width are available
 # as environment variable
 
 #ISSUE_URL = env['ISSUE_URL']
-#API_URL = env['GITHUB_API_URL']
 print(environ)
