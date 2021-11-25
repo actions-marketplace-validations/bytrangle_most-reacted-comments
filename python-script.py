80e69baab890a3212589c104dfba652dcddc3d4a
Trang Le <trang.thule@zoho.com>
2021-11-25 17:04:56 +0700

    debug: cast min comment number to integer

--- python-script.py
+++ python-script.py
@@ -2,8 +2,8 @@ from os import environ, getenv
 import re
 import requests
 
-minIssueComment = getenv('INPUT_MIN_ISSUE_COMMENT', 20)
-# print(minIssueComment)
+minIssueComment = int(getenv('INPUT_MIN_ISSUE_COMMENT', 20))
+print(minIssueComment)
 API_URL = getenv('GITHUB_API_URL', 'https://api.github.com')
 # print(API_URL)
 # Input parameter passed to jobs.<job_id>.steps[*].width are available
