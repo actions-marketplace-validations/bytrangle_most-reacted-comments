c0009c552aac562b4e216eca661516fbd0d6d2e0
Trang Le <trang.thule@zoho.com>
2021-11-26 17:42:35 +0700

    fix(script): correct env variable for max reacted comments

--- python-script.py
+++ python-script.py
@@ -2,11 +2,11 @@ from os import environ, getenv
 import re
 import requests
 
-minIssueComment = int(getenv('INPUT_MIN_ISSUE_COMMENT', 10))
-print(minIssueComment)
 API_URL = getenv('GITHUB_API_URL', 'https://api.github.com')
 # print(API_URL)
-MAX_REACTED_COMMENT = int(getenv('MAXIMUM_REACTED_COMMENT', 5))
+minIssueComment = int(getenv('INPUT_MIN_ISSUE_COMMENT', 10))
+print(minIssueComment)
+MAX_REACTED_COMMENTS = int(getenv('INPUT_MAXIMUM_REACTED_COMMENTS', 5))
 # Input parameter passed to jobs.<job_id>.steps[*].width are available
 # as environment variable
 
@@ -34,8 +34,8 @@ if (len(json) > minIssueComment):
       reactedComment = { "url": comment["html_url"], "body": extract, "reactionCount": reactionCount}
       commentList.append(reactedComment)
   commentList = sorted(commentList, key = lambda i: i["reactionCount"], reverse=True)
-  if (len(commentList) > MAX_REACTED_COMMENT):
-    commentList = commentList[:MAX_REACTED_COMMENT]
+  if (len(commentList) > MAX_REACTED_COMMENTS):
+    commentList = commentList[:MAX_REACTED_COMMENTS]
   print(commentList)
 else:
   print('exit')
